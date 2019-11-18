16dbdfd9835f8952ca0ebcbd29bd1a7d88c8f365
Lukáš Novák <novalu@novalu.info>
2019-11-18 11:09:13 +0100

    Reorder container state ids.

--- src/model/container_state/ContainerState.ts
+++ src/model/container_state/ContainerState.ts
@@ -1,14 +1,14 @@
 class ContainerState {
-    public static readonly RUNNING_UNKNOWN =
-        new ContainerState(0, "unknown", "#AAAAAA"); // container is running and health state not found
-    public static readonly RUNNING_HEALTHY =
-        new ContainerState(1, "healthy", "#2EB886"); // container is running and health is: healthy
-    public static readonly RUNNING_UNHEALTHY =
-        new ContainerState(2, "unhealthy", "#FF4454");// container is running and health is: starting
     public static readonly RUNNING_STARTING =
-        new ContainerState(3, "starting", "#000000"); // container is running and health is: unhealthy
+        new ContainerState(1, "starting", "#000000"); // container is running and health is: unhealthy
+    public static readonly RUNNING_HEALTHY =
+        new ContainerState(2, "healthy", "#2EB886"); // container is running and health is: healthy
+    public static readonly RUNNING_UNHEALTHY =
+        new ContainerState(3, "unhealthy", "#FF4454");// container is running and health is: starting
     public static readonly DOWN =
         new ContainerState(4, "down", "#FF4454"); // container is not running
+    public static readonly RUNNING_UNKNOWN =
+        new ContainerState(5, "unknown", "#AAAAAA"); // container is running and health state not found
 
     constructor(
         public id: number,
